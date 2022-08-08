626c428d7e4d49f49f7a82000b8a20e7553051c6
RuslanKos1989 <arrusl@yandex.ru>
2022-08-08 18:03:44 +0300

    base html and css for signup form

--- templates/sign_up.php
+++ templates/sign_up.php
@@ -1,7 +1,33 @@
 <div class="rgbcode-authform-modal">
-	<button class="rgbcode-authform-modal__close" type="button">X</button>
-	<div class="rgbcode-authform-signup">
+	<button class="rgbcode-authform-modal__close" type="button"></button>
+	<form class="rgbcode-authform-signup">
 		<div class="rgbcode-authform-signup__title">Sign up and Start Trading</div>
 		<div class="rgbcode-authform-signup__subtitle">Please fill in your details below</div>
-	</div>
+		<div class="rgbcode-authform-signup__inputs">
+			<label class="rgbcode-authform-input rgbcode-authform-input_user">
+				<input type="text" name="first_name" placeholder="First name">
+			</label>
+			<label class="rgbcode-authform-input rgbcode-authform-input_user">
+				<input type="text" name="last_name" placeholder="Last name">
+			</label>
+			<label class="rgbcode-authform-input rgbcode-authform-input_email">
+				<input type="email" name="email" placeholder="Email">
+			</label>
+			<div class="rgbcode-authform-flag-input">+ 46</div>
+			<label class="rgbcode-authform-input rgbcode-authform-input_phone">
+				<input type="text" maxlength="17" minlength="5" name="phone" placeholder="Phone">
+			</label>
+			<label class="rgbcode-authform-input rgbcode-authform-input_key">
+				<input type="password" name="password" placeholder="Password">
+			</label>
+		</div>
+		<label class="rgbcode-authform-checkbox">
+			<input type="checkbox" class="rgbcode-authform-checkbox__input">
+			<span class="rgbcode-authform-checkbox__box"></span>
+			By checking this box I accept the <a href="">Terms and Conditions</a> and confirm that I am over 18 years of age.
+		</label>
+		<button class="rgbcode-authform-button" type="submit" disabled>Submit</button>
+		<p class="rgbcode-authform-text rgbcode-authform-text_center">CFD and Forex trading involves substantial risk and may result in the loss of the invested capital.</p>
+		<a class="rgbcode-authform-signup__link" href="">Already have an account?</a>
+	</form>
 </div>
