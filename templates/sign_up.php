aa0c82873ed82b8473aa01fe61e4fabef6ed84f4
RuslanKos1989 <arrusl@yandex.ru>
2022-08-08 11:22:56 +0300

    add base templates for forms

---
+++ templates/sign_up.php
@@ -0,0 +1,7 @@
+<div class="rgbcode-authform-modal">
+	<button class="rgbcode-authform-modal__close" type="button">X</button>
+	<div class="rgbcode-authform-signup">
+		<div class="rgbcode-authform-signup__title">Sign up and Start Trading</div>
+		<div class="rgbcode-authform-signup__subtitle">Please fill in your details below</div>
+	</div>
+</div>
