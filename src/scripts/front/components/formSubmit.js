a538416d3a08d240914e5772931294fde9d16722
RuslanKos1989 <arrusl@yandex.ru>
2022-08-16 09:39:08 +0300

    add preloader for form

--- src/scripts/front/components/formSubmit.js
+++ src/scripts/front/components/formSubmit.js
@@ -2,6 +2,8 @@ import {postData, serializeArray} from "./utils";
 
 const formSignUp = document.querySelector( '.rgbcode-authform-signup' );
 const phoneCountry = formSignUp.querySelector( '.rgbcode-authform-flag-input__code' );
+const submitBtn = formSignUp.querySelector( '.rgbcode-authform-button' );
+const errorBlock = formSignUp.querySelector( '.rgbcode-authform-input__error_submit' );
 
 export function initFormSubmit() {
 	formSignUp.addEventListener( 'submit', ( evt ) => {
@@ -10,15 +12,17 @@ export function initFormSubmit() {
 		const data = serializeArray( formSignUp );
 		data.phonecountry = phoneCountry.textContent.trim();
 		data.iso = phoneCountry.dataset.iso;
+		submitBtn.classList.add( 'rgbcode-authform-button_loader' );
 
 		postData( '/wp-json/rgbcode/v1/create_account', data )
 			.then( data => {
 				if ( data.success ) {
 					location.href = data.link;
+					errorBlock.classList.add( 'rgbcode-hidden' );
 				} else {
-					const errorBlock = formSignUp.querySelector( '.rgbcode-authform-input__error_submit' );
 					errorBlock.classList.remove( 'rgbcode-hidden' );
 					errorBlock.textContent = data.message ? data.message : data.data;
+					submitBtn.classList.remove( 'rgbcode-authform-button_loader' );
 				}
 			} )
 			.catch( ( error ) => {
