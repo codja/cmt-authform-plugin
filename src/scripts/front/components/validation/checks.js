d7a72d533ee9c93928991e1bbfa5e586030050f0
RuslanKos1989 <arrusl@yandex.ru>
2022-10-12 16:29:51 +0300

    change minimal quantity characters for first and last names

--- src/scripts/front/components/validation/checks.js
+++ src/scripts/front/components/validation/checks.js
@@ -4,7 +4,7 @@ export const checkIfOneLowercase = i => /[a-z]/.test( i );
 export const checkIfOneUppercase = i => /[A-Z]/.test( i );
 export const checkIfOneDigit = i => /[0-9]/.test( i );
 export const checkRepeatedChars = i => /([a-z])\1{2}/ig.test( i );
-export const nameTest = i => /^[a-zA-Zء-ي]{4,50}$/.test( i );
+export const nameTest = i => /^[a-zA-Zء-ي]{3,50}$/.test( i );
 export const emailTest = i => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test( i );
 export const phoneTest = i => /^[0-9]{6,}$/.test( i );
 export const checkSeriesKeyboardChars = s => {
