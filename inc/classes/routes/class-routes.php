c7803311c6472cde3678056c1176d3aad2f532b3
RuslanKos1989 <arrusl@yandex.ru>
2023-06-12 12:31:19 +0300

    [ CMT-367-detect-country-ajax ] added detecting location through ajax

--- inc/classes/routes/class-routes.php
+++ inc/classes/routes/class-routes.php
@@ -3,9 +3,9 @@
 namespace Rgbcode_authform\classes\routes;
 
 use Rgbcode_authform\classes\routes\api\Create_Account;
+use Rgbcode_authform\classes\routes\api\Detect_Location;
 
 class Routes {
-
 	public function __construct() {
 		add_action(
 			'rest_api_init',
@@ -13,10 +13,24 @@ class Routes {
 				register_rest_route(
 					'rgbcode/v1',
 					'/create_account',
-					array(
+					[
 						'methods'  => 'POST',
 						'callback' => [ new Create_Account(), 'post' ],
-					)
+					]
+				);
+			}
+		);
+
+		add_action(
+			'rest_api_init',
+			function () {
+				register_rest_route(
+					'rgbcode/v1',
+					'/detect_location',
+					[
+						'methods'  => 'GET',
+						'callback' => [ new Detect_Location(), 'get' ],
+					]
 				);
 			}
 		);
--- src/scripts/front/components/flagSelect.js
+++ src/scripts/front/components/flagSelect.js
@@ -1,11 +1,34 @@
+import {postData} from "./utils.js";
+
 const modal = document.querySelector( '.rgbcode-authform-modal' );
+const notAllowedMsg = modal.querySelector( '.rgbcode-authform-message' );
 const flagInput = modal.querySelector( '.rgbcode-authform-flag-input' );
 const select = flagInput.querySelector( '.rgbcode-authform-flag-input__select' );
 const options = flagInput.querySelectorAll( '.rgbcode-authform-flag-input__option' );
 const flagImg = flagInput.querySelector( '.rgbcode-authform-flag-input__flag' );
 const telephoneCode = flagInput.querySelector( '.rgbcode-authform-flag-input__code' );
 
+const fillFlagInput = ( data ) => {
+	flagImg.src = data.src;
+	telephoneCode.dataset.iso = data.iso;
+	telephoneCode.textContent = data.code;
+}
+
 export function initFlagSelect() {
+
+	postData( '/wp-json/rgbcode/v1/detect_location', {}, 'GET' )
+		.then( response => {
+			if ( response.success ) {
+				if ( response.data.not_allowed ) {
+					notAllowedMsg.classList.add( 'rgbcode-hidden' );
+				}
+				fillFlagInput( response.data.country );
+			}
+		} )
+		.catch( ( error ) => {
+			console.error('Error:', error);
+		} );
+
 	flagInput.addEventListener( 'click', () => {
 		select.classList.toggle( 'rgbcode-hidden' );
 		modal.classList.toggle( 'rgbcode-authform-modal_overflow' );
@@ -13,9 +36,7 @@ export function initFlagSelect() {
 
 	options.forEach( option => {
 		option.addEventListener( 'click', () => {
-			flagImg.src = option.dataset.src;
-			telephoneCode.dataset.iso = option.dataset.iso;
-			telephoneCode.textContent = option.dataset.code;
+			fillFlagInput( option.dataset );
 		} );
 	} )
 }
