5bfacefb0f8780e68fa71ae5d2e6fd89fbc97f73
RuslanKos1989 <arrusl@yandex.ru>
2024-11-10 11:54:22 +0300

    [ CMT-479-antelope-crm ] updated login.

--- inc/classes/providers/antelope/requests/class-antelope-login.php
+++ inc/classes/providers/antelope/requests/class-antelope-login.php
@@ -2,29 +2,22 @@
 
 namespace Rgbcode_authform\classes\providers\antelope\requests;
 
-use Rgbcode_authform\classes\providers\antelope\Antelope_Affiliate;
-use Rgbcode_authform\classes\providers\antelope\Antelope_CRM;
+use Rgbcode_authform\classes\providers\antelope\Antelope_Client;
 use Rgbcode_authform\interfaces\CRM_Endpoint;
 
 class Antelope_Login implements CRM_Endpoint {
 
-	public function get_endpoint(): string {
-		return Antelope_Affiliate::BASE_URL_API . 'getUser';
-	}
-
-	public function get_body( $data ): array {
-
-		return [
-			'userid' => absint( $data['user_id'] ?? '' ),
-			'apikey' => ANTILOPE_API_AFFILIATE_KEY,
-		];
-	}
+	const REDIRECT_BASE_URL = 'https://myaccount.cmtrading.com/#/authentication/auth/';
+	const DEFAULT_LINK = 'https://myaccount.cmtrading.com/#/login';
 
 	public function get_result( $response, $request ): array {
+		$link = ! empty( $response['result']['authToken'] )
+			? self::REDIRECT_BASE_URL . $response['result']['authToken']
+			: self::DEFAULT_LINK;
 
 		return [
 			'success' => $response['success'] ?? false,
-			'link'    => $response['result'] ?? '',
+			'link'    => $link,
 		];
 	}
 
@@ -34,41 +27,26 @@ class Antelope_Login implements CRM_Endpoint {
 		}
 
 		// get userid by email
-		$antelope_crm = new Antelope_CRM();
-		$response     = $antelope_crm->send_request(
-			'user-id',
-			[
-				'email' => $data['email'] ?? '',
-			]
-		);
-		$antelope_crm->check_response( $response );
-
-		// get userdata from CRM
-		$user_id   = $response['result'] ?? null;
-		$user_data = $provider->send_request(
-			$this,
-			$this->get_body( [ 'user_id' => $user_id ] )
-		);
-		$provider->check_response( $user_data );
-
-		// checking password
-		$password_hash = $user_data['result']['password'] ?? null;
-		$user_password = $data['password'] ?? '';
-		if ( ! password_verify( $user_password, $password_hash ) ) {
-			wp_send_json_error( esc_html__( 'Incorrect Data', 'rgbcode-authform' ) );
-		}
-
-		// get autologin link(regenerated)
-		$regenerate_autologin = new Regenerate_Autologin();
-		$regenerate_data      = $antelope_crm->send_request(
-			$regenerate_autologin->get_endpoint(),
-			$regenerate_autologin->get_body( [ 'user_id' => $user_id ] ),
+		$api      = new Antelope_Client();
+		$response = $api->send_request(
+			$this->get_endpoint(),
+			$this->get_body( $data ),
 			'POST',
 			true
 		);
-		$antelope_crm->check_response( $regenerate_data );
+		$api->check_response( $response );
 
-		return $regenerate_data;
+		return $response;
 	}
 
+	public function get_endpoint(): string {
+		return 'users/authenticate';
+	}
+
+	public function get_body( $data ): array {
+		return [
+			'email'    => $data['email'] ?? '' ,
+			'password' => $data['password'] ?? '',
+		];
+	}
 }
